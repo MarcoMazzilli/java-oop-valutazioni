cfa2e08cab74e8f0f13e96203afac4a6418ee52b
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-09-15 17:29:07 +0200

    [Add]Loop to create fake students

--- src/org/lessons/java/valutazioni/Main.java
+++ src/org/lessons/java/valutazioni/Main.java
@@ -1,15 +1,22 @@
 package org.lessons.java.valutazioni;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Random rnd = new Random();
+				
+		Studente[] arrayStudenti = new Studente[20];
 		
-		Studente student1 = new Studente();
-		
-		if(student1.getStatus()) {
-			System.out.println("promosso");
-		}else {
-			System.out.println("Bocciato");
+		for (int i = 0; i < arrayStudenti.length; i++) {
+			
+			float mediaVoti = rnd.nextFloat(1,6);
+			int percentualeAssenze = rnd.nextInt(0,100);
+			
+			arrayStudenti[i] = new Studente(i,percentualeAssenze,mediaVoti);
+			
+			arrayStudenti[i].getInfoStudent();
 		}
 	}
 	
--- src/org/lessons/java/valutazioni/Studente.java
+++ src/org/lessons/java/valutazioni/Studente.java
@@ -6,21 +6,17 @@ public class Studente {
 	public int percentualeAssenze;
 	public float mediaVoti;
 	
-//	Studente(int id){
-//		this.id = id; 
-//	}
-	
-	Studente(){
-		this.id = 5; 
-		this.percentualeAssenze = 24;
-		this.mediaVoti = 2.5f;
+	Studente(int id , int percentualeAssenze, float mediaVoti){
+		this.id = id;
+		this.percentualeAssenze = percentualeAssenze;
+		this.mediaVoti = mediaVoti;
 	}
 	
 
-	public boolean getStatus() {
+	public String getStatus() {
 		boolean promosso = false;
 		
-		if(percentualeAssenze > 25 && percentualeAssenze < 50 && mediaVoti > 2) {
+		if(percentualeAssenze >= 25 && percentualeAssenze <= 50 && mediaVoti > 2) {
 			promosso = true;
 		}
 		
@@ -32,7 +28,15 @@ public class Studente {
 			promosso = false;
 		}
 		
-		return promosso;
+		return promosso? "Promosso":"Bocciato";
+	}
+	
+	public void getInfoStudent() {
+		System.out.println("ID Studente: "+ this.id);
+		System.out.println("Media voti: " + String.format("%.2f", this.mediaVoti));
+		System.out.println("Percentuale assenze: " + this.percentualeAssenze + "%");
+		System.out.println("Stato studente: " + this.getStatus());
+		System.out.println("-------------");
 	}
 	
 }
